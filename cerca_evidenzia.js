93aaf1eb53dfbcb2709aeac1feda04ba1c9e4d43
DaniloZoia <dana_ilo@yahoo.it>
2018-11-28 16:09:50 +0100

    Delete cerca_evidenzia.js

--- cerca_evidenzia.js
+++
@@ -1,8 +0,0 @@
-$('#search').keyup(function(){
-		 var par = $("p").text();
-         $("p").text(par);
-         $1 = $('#search').val();   
-         $("p").html((g, html) => {
-			 return html.split($1).join("<span>" + $1 + "</span>");
-         });
-});
